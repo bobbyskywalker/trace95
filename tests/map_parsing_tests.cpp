f79ed9229b7be84a7637f5e2c05b934a3faa2598
Olek <aleksander.garbacz@gmail.com>
2025-06-24 16:51:53 +0200

    map parsing google tests wip

--- tests/map_parsing_tests.cpp
+++ tests/map_parsing_tests.cpp
@@ -1,1 +1,44 @@
+#include <gtest/gtest.h>
+#include "../inc/GameMap.hpp"
+
+TEST(GameMap, ValidSimpleFile) {
+
+}
+
+TEST(GameMap, ValidMultiTabFile) {
+
+}
+
+TEST(GameMap, InvalidSimpleFile) {
+
+}
+
+TEST(GameMap, InvalidNoTexture) {
+
+}
+
+TEST(GameMap, InvalidWrongColor) {
+
+}
+
+TEST(GameMap, InvalidMissingColors) {
+
+}
+
+TEST(GameMap, InvalidUnclosedMap) {
+
+}
+
+TEST(GameMap, InvalidNoMap) {
+
+}
+
+TEST(GameMap, NonExistentTextures) {
+
+}
+
+TEST(GameMap, FileNonExistent) {
+	EXPECT_THROW(GameMap gm("nonexistent_file.cub"), std::runtime_error);
+	EXPECT_THROW(GameMap gm("erjkfhfiuhru"), std::runtime_error);
+}
 
